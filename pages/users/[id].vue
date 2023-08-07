cc41b105b64a355a0089a97bac479d45b346f0a3
Raz <me@raz.wtf>
2023-08-07 15:50:21 +0200

    params validation error with zod

--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 const route = useRoute()
 const { data: user } = await useFetch(`/api/users/${route.params.id}`)
+if (!user.value)
+  throw createError({ statusCode: 404, statusMessage: 'Page Not Found', message: 'User Not Found' })
 </script>
 
 <template>
--- server/api/users/[id].get.ts
+++ server/api/users/[id].get.ts
@@ -1,13 +1,20 @@
 import { zh } from 'h3-zod'
+import { fromZodError } from 'zod-validation-error'
 import { selectUser } from '@/database/users/handlers'
 
 export default defineEventHandler(async (event) => {
-  const params = await zh.useValidatedParams(event, {
+  const params = await zh.useSafeValidatedParams(event, {
     id: zh.intAsString,
   })
-
-  const idParam = params.id
-
-  const user = await selectUser(idParam)
-  return user
+  if (!params.success) {
+    const validationError = fromZodError(params.error)
+    throw createError({
+      statusCode: 400,
+      message: validationError.toString(),
+    })
+  }
+  else {
+    const user = await selectUser(params.data.id)
+    return user
+  }
 })
