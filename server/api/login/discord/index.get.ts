6352e84956ab2e5ccf5d016c000a13996f3c63ff
Raz <me@raz.wtf>
2023-08-27 00:37:52 +0200

    lint:

--- server/api/login/discord/index.get.ts
+++ server/api/login/discord/index.get.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async (event) => {
   const authRequest = auth.handleRequest(event)
   const session = await authRequest.validate()
   if (session)
-   return sendRedirect(event, '/user')
+    return sendRedirect(event, '/user')
 
   const [url, state] = await discordAuth.getAuthorizationUrl()
   setCookie(event, 'discord_oauth_state', state, {
--- server/api/user/index.put.ts
+++ server/api/user/index.put.ts
@@ -3,7 +3,6 @@ import { z } from 'zod'
 const usernameSchema = z.object({
   username: z.string().min(3).max(20),
 })
-type UsernameSchema = z.infer<typeof usernameSchema>
 
 export default defineEventHandler(async (event) => {
   const authRequest = auth.handleRequest(event)
