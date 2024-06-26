15a25c5397f30bdea7d3941890a9a763d730a4d1
Raz <me@raz.wtf>
2024-06-26 22:57:10 +0200

    remove unused process.env

--- server/utils/database/index.ts
+++ server/utils/database/index.ts
@@ -1,12 +1,9 @@
-import process from 'node:process'
 import { drizzle } from 'drizzle-orm/d1'
 import { sessionTable, todoTable, userTable } from './schema'
 
-const config = process.env
-
 const sql = hubDatabase()
 
-const db = drizzle(sql, { schema: { ...userTable, ...todoTable, ...sessionTable }, logger: config.NODE_ENV === 'development' })
+const db = drizzle(sql, { schema: { ...userTable, ...todoTable, ...sessionTable } })
 
 export * as handlers from './handlers'
 export * as schemas from './schema'
