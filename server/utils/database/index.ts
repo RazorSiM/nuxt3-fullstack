4e7d20c6b77eb1ddc5dcbcb38305528fc430b5a8
Raz <me@raz.wtf>
2024-06-26 21:37:58 +0200

    use drizzle-orm/d1

--- server/utils/database/index.ts
+++ server/utils/database/index.ts
@@ -1,13 +1,10 @@
 import process from 'node:process'
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
+import { drizzle } from 'drizzle-orm/d1'
 import { sessionTable, todoTable, userTable } from './schema'
 
 const config = process.env
-const connectionString = `postgresql://${config.NUXT_POSTGRES_USER}:${config.NUXT_POSTGRES_PASSWORD}@${config.NUXT_POSTGRES_HOST}:${config.NUXT_POSTGRES_PORT}/${config.NUXT_POSTGRES_DATABASE}`
 
-// @ts-expect-error if the module is not loaded, hubDatabase cannot be called
-const sql = process.env.NUXT_HUB_ENABLED ? hubDatabase() : postgres(connectionString)
+const sql = hubDatabase()
 
 const db = drizzle(sql, { schema: { ...userTable, ...todoTable, ...sessionTable }, logger: config.NODE_ENV === 'development' })
 
